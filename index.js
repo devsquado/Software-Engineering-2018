a8131a567e2bb682ec3935adb2a5d864015e274e
devsquado <36514845+devsquado@users.noreply.github.com>
2018-02-21 22:40:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+export default class LogPage extends React.Component {
+    render(){
+       return(
+               logpage
+        )}
+}
+
+ReactDOM.render(<LogPage />, document.getElementById('log-page'));
