6e586fdf440a6dabdd78cdce13a8d01b6df6b4e5
devsquado <36514845+devsquado@users.noreply.github.com>
2018-03-09 22:56:12 +0300

    Create user.js

---
+++ controllers/user.js
@@ -0,0 +1,81 @@
+const Usermodel = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+var loginpage = function(req,res) {
+    res.render('index')
+  }
+
+var create = function (req, res, next) {
+    
+      console.log(req.body.email)
+    
+      bcrypt.genSalt(10,function(err, salt){
+        bcrypt.hash(req.body.password, salt, function(err,hash){
+         console.log(hash);
+          var user = new Usermodel({
+            name: req.body.username,
+            email: req.body.email,
+            password: hash
+          
+            
+            });
+    
+            user.saveAsync()
+            .then(function(savedUser) {
+                return res.status(200).send('kayıt tamam ' + (user.name))
+              })
+              .catch(function (err) {
+                return res.render('404', {
+                  hata_kodu: err
+                });
+              });
+        });
+      });
+    
+ };
+
+var login = function (req, res, next){
+    
+      var username= req.body.username;
+      var email= req.body.email;
+      var logpassword = req.body.password;
+      
+          Usermodel.findOne({name: username, email: email}, function(err,user){
+            
+            if(err){
+              console.log(err);
+              return res.status(500).send();
+            }
+            if(!user){
+              return res.render('404',{
+                hata_kodu: err
+              })
+            }
+            bcrypt.compare(logpassword,user.password,function(err,result){
+              if(err){
+                return res.send(err)
+              }
+              if(result){
+                req.session.user = user;
+                return res.redirect('/usersList');
+              }
+              return res.render('404');
+           
+            
+          })
+  
+         }
+          )} 
+
+var logout= function(req, res){
+    req.session.destroy();
+    return res.redirect('/');
+    
+
+            }         
+            
+            
+exports.create = create; 
+exports.login = login;
+exports.loginpage = loginpage;
+exports.logout = logout;
