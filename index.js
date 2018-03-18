16710d6526e707d3674e4d7dbb3788940a4811bd
devsquado <36514845+devsquado@users.noreply.github.com>
2018-03-18 21:26:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 const express = require('express');
 const userRoutes = require('./routes/route')
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const routes = require('./routes/api');
 const Promise = require('bluebird');
+
 
 const app = express();
 
 mongoose.connect('mongodb://localhost/usergo');
-    
+
 Promise.promisifyAll(mongoose);
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// parse application/json
 app.use(bodyParser.json());
 
-app.use(routes);
+app.use(session({ secret: 'ui2sajhsadaytrop', name: 'melih', resave: false, saveUninitialized: true, cookie: { httpOnly: true, expires: new Date(Date.now() + (40000000)) } }));
 
-app.get('/',function(){
+
+app.use('/', routes);
+
+app.get('/', function () {
 
 })
 
-app.listen(process.env.port || 1234,function(){
+
+
+app.listen(process.env.port || 1234, function () {
     console.log("Working!")
 })
