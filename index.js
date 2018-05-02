b43a8f3db0e5df1b6c5edd2c68f6a57bdd8b254d
devsquado <36514845+devsquado@users.noreply.github.com>
2018-05-03 00:49:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const routes = require('./routes/api');
 const Promise = require('bluebird');
-
+const nodemailer = require('nodemailer');
+const async = require('async');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('express-flash');
+const bcrypt = require('bcryptjs');
+const socket = require('socket.io')
 
 const app = express();
+
+var server = app.listen(process.env.port || 1234, function () {
+    console.log("Working!")
+})
+
+
 
 mongoose.connect('mongodb://localhost/usergo');
 
@@ -19,7 +31,13 @@ app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
+  
+  
+var io = socket(server);
 
+io.on('connection',function(){
+    console.log('made socket connection',socket.id);
+})
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -30,6 +48,10 @@ app.use(bodyParser.json());
 
 app.use(session({ secret: 'ui2sajhsadaytrop', name: 'melih', resave: false, saveUninitialized: true, cookie: { httpOnly: true, expires: new Date(Date.now() + (40000000)) } }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use('/', routes);
 
@@ -39,6 +61,4 @@ app.get('/', function () {
 
 
 
-app.listen(process.env.port || 1234, function () {
-    console.log("Working!")
-})
+
