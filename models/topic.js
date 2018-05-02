996a0209a1856009819efab0cfb2d10612072611
ABMecik <35038370+ABMecik@users.noreply.github.com>
2018-05-02 20:31:33 +0300

    Add files via upload

---
+++ models/topic.js
@@ -0,0 +1,10 @@
+﻿const mongoose = require('mongoose');
+
+const TopicSchema = new mongoose.Schema({
+  title: String,
+  name: String,
+  comment: String,
+  created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Topic', TopicSchema);
