e64d1f1998116e74c19069b1e02c44728d2f9ee2
devsquado <36514845+devsquado@users.noreply.github.com>
2018-05-03 00:50:09 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const userCtrl = require('../controllers/user');
-const app=express();
+const app = express();
 const router = express.Router();
 
-module.exports = (function() {
+module.exports = (function () {
 
   router.post('/register', userCtrl.create);
   router.get('/homepage', userCtrl.homepage);
+  router.post('/newpass',userCtrl.newpass);
+  router.post('/forgot',userCtrl.forgot);
+  router.get('/profile', userCtrl.profile);
   router.post('/login', userCtrl.login);
   router.get('/loginpage', userCtrl.loginpage);
-  router.get('/logout',userCtrl.logout);
-  router.get('/profile', userCtrl.profile);
+  router.get('/logout', userCtrl.logout);
+  router.get('/reset/:token',userCtrl.reset);
   router.get('/account_settings', userCtrl.account_settings);
   router.get('/profile_settings', userCtrl.profile_settings);
   router.get('/contact', userCtrl.contact);
   router.get('/main', userCtrl.main);
   router.get('/topic', userCtrl.topic);
-  
+  router.get('/chat', userCtrl.chat);
 
-  return router })();
+  return router
+})();
