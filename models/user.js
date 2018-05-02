7c863d5f3d778dd1d63d2018a9b050b1ac795e88
devsquado <36514845+devsquado@users.noreply.github.com>
2018-05-03 00:50:51 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String , required: true, unique: true},
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now },
+    gender: {type: String},
+    friendlist: {type: Array},
+    interests: {type: Array},
+    resetPasswordToken: {type: String},
+    resetPasswordExpires: {type: Date}
+
 })
+
 
 module.exports = mongoose.model('User', UserSchema);
